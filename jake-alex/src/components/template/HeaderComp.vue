63914ffe4a675d341f39cf6aaf69f2ff3ef5b9ae
José Gabriel Corrêa <101403016+GhostWisher13@users.noreply.github.com>
2022-06-27 09:09:48 -0300

    Add files via upload

---
+++ jake-alex/src/components/template/HeaderComp.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <div class="menu-esquerdo">
+      <span> <RouterLink to="/">Livros</RouterLink> </span>
+      <span>|</span>
+      <span> <RouterLink to="/times">Autores</RouterLink> </span>
+      <span>|</span>
+      <span> <RouterLink to="/jogadores">Mais Vendidos</RouterLink> </span>
+    </div>
+    <div class="menu-direito">
+      <input type="text" v-model="pesquisa" />
+    </div>
+  </header>
+</template>
+
+<style>
+header span {
+  padding: 0 20px;
+}
+
+header {
+  background-color: #000000;
+  height: 15%;
+  font-size: 1.3rem;
+  padding-left: 2rem;
+  color: rgb(255, 0, 0);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
